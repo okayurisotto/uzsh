f89adf2dec8aab35fe84a45d096380e8adf6749a
okayurisotto <okayurisotto@proton.me>
2024-01-26 17:04:52 +0900

    バックアップの作成と読み込み機能の追加

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,3 +1,4 @@
+import { createRef } from "preact";
 import { useEffect, useState } from "preact/hooks";
 import { StorageKey, StorageSchema, UserEntrySchema } from "../const";
 import { z } from "zod";
@@ -38,11 +39,92 @@ export const App: preact.FunctionComponent<Empty> = () => {
     });
   };
 
+  const handleExport = () => {
+    const data = {
+      version: 1,
+      entries: entries.map((entry) => ({ ...entry, url: entry.url.source })),
+    };
+    const blob = new Blob([JSON.stringify(data)], {
+      type: "application/json",
+    });
+    const url = URL.createObjectURL(blob);
+    const now = new Date();
+    const filename =
+      [
+        "uzsh",
+        [
+          now.getFullYear().toString().padStart(4, "0"),
+          (now.getMonth() + 1).toString().padStart(2, "0"),
+          now.getDate().toString().padStart(2, "0"),
+        ].join("-"),
+        [
+          now.getHours().toString().padStart(2, "0"),
+          now.getMinutes().toString().padStart(2, "0"),
+          now.getSeconds().toString().padStart(2, "0"),
+        ].join("-"),
+      ].join("_") + ".json";
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  };
+
+  const handleImport = async (file: File) => {
+    const json = await file.text();
+
+    const data = z
+      .object({
+        version: z.literal(1),
+        entries: UserEntrySchema.extend({ url: z.string() }).array(),
+      })
+      .parse(JSON.parse(json));
+
+    setEntries(
+      [
+        ...data.entries.map((entry) => ({
+          ...entry,
+          url: new RegExp(entry.url),
+        })),
+        ...entries,
+      ].filter((entry, index, source) => {
+        return source.findIndex((current) => current.id === entry.id) === index;
+      }),
+    );
+  };
+
+  const importFileInputRef = createRef<HTMLInputElement>();
   return (
     <>
       <div class="global-controls">
         <button onClick={handleAdd}>エントリを新規作成</button>
         <button onClick={() => void handleSave()}>設定を保存</button>
+        <button onClick={handleExport}>設定をエクスポート</button>
+        <button
+          onClick={() => {
+            importFileInputRef.current?.click();
+          }}
+        >
+          設定をインポート
+        </button>
+        <input
+          ref={importFileInputRef}
+          type="file"
+          multiple={false}
+          style={{ display: "none" }}
+          onChange={(e) => {
+            if (e.currentTarget.files === null) return;
+
+            const [file] = e.currentTarget.files;
+            if (file === undefined) return;
+
+            return void (async () => {
+              await handleImport(file);
+            })();
+          }}
+        />
       </div>
       {entries.map((value) => (
         <UserEntryForm
